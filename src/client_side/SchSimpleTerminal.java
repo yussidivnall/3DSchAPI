05b0dd5509ed28a33bcf38b4652f73d757c05d10
uri <UC.dev.null@gmail.com>
2009-09-17 07:25:02 +0100

    some small additions, still needs to figure out a good way to read replay in SchSimpleTerminal

--- src/client_side/SchSimpleTerminal.java
+++ src/client_side/SchSimpleTerminal.java
@@ -1,7 +1,7 @@
 import javax.net.ssl.*;
 import java.io.*;
 public class SchSimpleTerminal{
-
+	
 	public static void main(String[] args){
 		try{
 			SSLSocketFactory fact = (SSLSocketFactory)SSLSocketFactory.getDefault();
@@ -14,10 +14,36 @@ public class SchSimpleTerminal{
 			OutputStreamWriter osw = new OutputStreamWriter(os);
 			BufferedWriter bw = new BufferedWriter(osw);
 			
+			BufferedReader sockBR = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			
 			String line = null;
 			while((line = br.readLine())!=null){
 				bw.write(line+'\n');
 				bw.flush();
+				
+				
+				String r = null;
+				
+				while ((r= sockBR.readLine())!=null){
+					System.out.println(r);
+					
+				}
+				
+			
+				
+				
+				//while ((r = sockBR.readLine())!=null){
+				//	System.out.println(r);
+				//}
+				//while ((r = new BufferedReader(new InputStreamReader(sock.getInputStream())).readLine()) != null){
+				//	System.out.println(r);
+				//}
+				System.out.println("Got out:)");
+				//BufferedReader sockBuff = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+				//while (sockBuff.ready()){
+				//	System.out.println(sockBuff.readLine());
+				//}
+				//System.out.println(new BufferedReader(new InputStreamReader(sock.getInputStream())).readLine());
 			}
 		}catch(IOException ioe){ioe.printStackTrace();}
 	}
--- src/server_side/SchProtocolHandler.java
+++ src/server_side/SchProtocolHandler.java
@@ -3,7 +3,8 @@ import java.io.*;
 
 public class SchProtocolHandler {
 	SSLSocket socket;
-	
+	boolean done = false;
+	PrintWriter pout;
 	
 	SchProtocolHandler(SSLSocket sock){ 
 		System.out.println("Protocol handler CONSTRUCTOR");
@@ -12,18 +13,42 @@ public class SchProtocolHandler {
 	public void RequestListener(){
 		System.out.println("Protocol handler starting...");
 		try{
-			InputStream istream= socket.getInputStream();
-			System.out.println("Got Input stream :) ...");
-			
-			InputStreamReader isr = new InputStreamReader(istream);
+			InputStream is= socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(isr);
+			pout = new PrintWriter(socket.getOutputStream(),true);
+			
 			String line = null;
 			System.out.println("Waiting for client input now...");
-			while((line=br.readLine())!=null){
+			while((line=br.readLine())!=null && !done ){
 				System.out.println(line);
 				System.out.flush();
+				parseLine(line);
 			}
+			
+			is.close();
+			//os.close();
 		}catch(IOException ioe){ioe.printStackTrace();}
 	}
-
+	public void parseLine(String line){
+		if(line.equals(new String("GETALL"))){
+			dumpAll();
+		}else if(line.startsWith("GETJOBS:")){
+			dumpJobs(line);
+		}else if (line.equals(new String("quit"))){done=true;}
+	}
+	
+	public void dumpAll(){
+		System.out.println("Dumping all map");
+		try{
+			
+			pout.println("GAGAGAGAG");
+			pout.println("GAGAGAGAG");
+			pout.println("GAGAGAGAG");
+			//pout.close();
+		}catch(Exception ioe){ioe.printStackTrace();}
+	}
+	public void dumpJobs(String request){
+		System.out.println("Dumping requested jobs:"+request);
+	}
 }
